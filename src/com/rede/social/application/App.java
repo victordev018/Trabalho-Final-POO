1aa48e4edab0e229da4004b728c813f4c278e6ba
João VIctor <joawrt8520@gmail.com>
2025-01-24 21:56:37 -0300

    feat: adiciona classe App e classe IOUtil com métodos auxiliares de entrada e saida

---
+++ src/com/rede/social/application/App.java
@@ -0,0 +1,19 @@
+package com.rede.social.application;
+
+public class App {
+
+    private SocialNetwork socialNetwork;
+
+    public App(SocialNetwork socialNetwork) {
+        this.socialNetwork = socialNetwork;
+    }
+
+    // métodos relacionados ao gerenciamento de perfis
+
+    // métodos relacionado ao gerenciamento de publicações
+
+    // métodos relacionado ao gerenciamento de solicitações
+
+    // métodos relacionado ao gerenciamento de interações
+
+}
---
+++ src/com/rede/social/util/IOUtil.java
@@ -0,0 +1,24 @@
+package com.rede.social.util;
+
+import java.util.Scanner;
+
+public class IOUtil {
+
+    private static Scanner in = new Scanner(System.in, "UTF-8").useDelimiter("\n");
+
+    public IOUtil(Scanner in) {}
+
+    public String getText(String message) {
+        System.out.println(message);
+        String result = in.nextLine();
+        return result;
+    }
+
+    public void showError(String error) {
+        System.out.println(error);
+    }
+
+    public void closeScanner() {
+        in.close();
+    }
+}
